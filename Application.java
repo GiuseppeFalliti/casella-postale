09b2057511da7b1517c181f84cb6025ccd561fa8
GiuseppeFalliti <141495925+GiuseppeFalliti@users.noreply.github.com>
2024-03-18 21:44:51 +0100

    Update Application.java

--- Application.java
+++ Application.java
@@ -12,11 +12,11 @@ public class Application extends Thread {
     private static final String POS_Y = "y";
     private JFrame frame;
     private Container cp;
-    private JTextArea posta1, posta2, posta3;
+    private static JTextArea posta1, posta2, posta3;
     private Preferences preferences;
     private final String MESSAGGE_IN_A_BOTTLE = "MESSAGGE_IN_A_BOTTLE";
-    private final String MESSAGGE_READ = "MESSAGGE_READ";
-    private AtomicBoolean Postabool = new AtomicBoolean(false);
+    private final static String MESSAGGE_READ = "MESSAGGE_READ";
+    private AtomicBoolean postaInUso = new AtomicBoolean(false);
 
     public Application() {
         super();
@@ -25,13 +25,13 @@ public class Application extends Thread {
         cp.setLayout(new FlowLayout(FlowLayout.CENTER));
         frame.setTitle("Application");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        preferences = Preferences.userNodeForPackage(Application.class);
+        preferences = Preferences.userNodeForPackage(Application3.class);
         int width = preferences.getInt(WIDTH_KEY, 300);
         int height = preferences.getInt(HEIGHT_KEY, 400);
         int posx = preferences.getInt(POS_X, 100);
         int posy = preferences.getInt(POS_Y, 100);
 
-        frame.setSize(800, 300);
+        frame.setSize(900, 300);
         frame.setLocation(posx, posy);
         frame.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
@@ -44,6 +44,7 @@ public class Application extends Thread {
     }
 
     private void setupApp() {
+        //impostazione layout delle JTextArea
         Border border = BorderFactory.createLineBorder(Color.BLACK, 2);
         posta1 = new JTextArea(7, 25);
         posta1.setText("Posta1");
@@ -51,13 +52,11 @@ public class Application extends Thread {
         posta1.setEditable(false);
         posta1.setBackground(new Color(255, 0, 0));
 
-
         posta2 = new JTextArea(7, 25);
         posta2.setBorder(border);
         posta2.setText("Posta2");
         posta2.setEditable(false);
         posta2.setBackground(new Color(255, 0, 0));
-
 
         posta3 = new JTextArea(7, 25);
         posta3.setBorder(border);
@@ -65,15 +64,18 @@ public class Application extends Thread {
         posta3.setEditable(false);
         posta3.setBackground(new Color(255, 0, 0));
 
+        //gestione degli eventi quando viene cliccato il bottone
         JButton startSimuleButton = new JButton("Start Simulation");
         startSimuleButton.addActionListener(e -> {
             Thread simulationThread = new Thread(() -> {
                 try {
                     Random random = new Random();
                     for (int i = 0; i < 10; i++) {
-                        int postaIndex = random.nextInt(3) + 1;
-        
-                        JTextArea targetPosta;
+                        int postaIndex = random.nextInt(3) + 1; // numero casuale che rappresenta le caselle.
+
+                        // viene scelta casualmente una delle 3 caselle e viene assegnata a una nuova
+                        // JTextArea così modifichiamo la JTextarea corrente subito.
+                        JTextArea targetPosta = null;
                         switch (postaIndex) {
                             case 1:
                                 targetPosta = posta1;
@@ -85,32 +87,32 @@ public class Application extends Thread {
                                 targetPosta = posta3;
                                 break;
                             default:
-                                throw new IllegalStateException("valore non valido: " + postaIndex);
+                                break;
                         }
-                        if (!Postabool.getAndSet(true)) {
+                        // controlla se la casella è vuota e aggiorna la casella a rosso.
+                        if (!postaInUso.getAndSet(true)) {
                             targetPosta.setText(MESSAGGE_IN_A_BOTTLE);
-                             targetPosta.setBackground(new Color(255, 0, 0));
+                            targetPosta.setBackground(new Color(255, 0, 0));
                         }
                         try {
                             Thread.sleep(1000); 
                         } catch (InterruptedException ex) {
                             ex.printStackTrace();
                         }
+                        // aggiorna la casella a verde se il messaggio è stato inviato.
                         if (targetPosta.getText().equals(MESSAGGE_IN_A_BOTTLE)) {
                             targetPosta.setText(MESSAGGE_READ);
-                           targetPosta.setBackground(new Color(0, 255, 0));
+                            targetPosta.setBackground(new Color(0, 255, 0));
                         }
-                        Postabool.set(false);
+                        postaInUso.set(false); // impostiamo il valore inziale a false.
                     }
                 } finally {
                     JOptionPane.showMessageDialog(frame, "Simulazione terminata");
                 }
             });
             simulationThread.start();
-           
         });
 
-        
         cp.add(posta1);
         cp.add(posta2);
         cp.add(posta3);
@@ -125,18 +127,72 @@ public class Application extends Thread {
     }
 
     public void startApp(boolean packElements) {
-        if (packElements) frame.pack();
+        if (packElements)
+            frame.pack();
         frame.setVisible(true);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new Application().startApp(false);
+
+            // Creazione thread che leggono i messaggi.
+            for (int i = 0; i < 3; i++) {
+                Thread readerThread = new Thread(new Reader("ciao" + (i + 1)));
+                readerThread.start();
+            }
         });
     }
 
     @Override
     public void run() {
         throw new UnsupportedOperationException("metodo non implementato");
+    }
+
+    // Classe interna per la lettura dei messaggi.
+    static class Reader implements Runnable {
+        private String name;
+
+        public Reader(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                // garantiamo che un thread alla volta possa accedere e modificare le risorse
+                // condivise all'interno della classe Application.
+                synchronized (Application.class) {
+                    JTextArea targetPosta = null;
+                    // Scegli casualmente una delle caselle di posta
+                    Random random = new Random();
+                    int postaIndex = random.nextInt(3) + 1;
+
+                    switch (postaIndex) {
+                        case 1:
+                            targetPosta = posta1;
+                            break;
+                        case 2:
+                            targetPosta = posta2;
+                            break;
+                        case 3:
+                            targetPosta = posta3;
+                            break;
+                        default:
+                            break;
+                    }
+                    // Se la casella contiene un messaggio da leggere
+                    if (targetPosta.getText().equals(MESSAGGE_READ)) {
+                        targetPosta.setText("Nome lettore: \n" + name + "\n  messaggio: \n" + targetPosta.getText());
+
+                    }
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 }
